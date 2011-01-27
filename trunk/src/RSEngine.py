90844a18c216333b5d5e4e229a4e7438cdc85687
rbulha@gmail.com <rbulha@gmail.com@e9923cd5-20db-f094-e8c5-cfe283a76e9a>
2011-01-27 21:01:41 +0000

    added input and output methods

--- trunk/src/RSEngine.py
+++ trunk/src/RSEngine.py
@@ -5,6 +5,7 @@ Created on 25/01/2011
 '''
 
 from RSClientServer import CRSServer
+from RSClientServer import CRSClient
 
 class CRSEngine(CRSServer):
     '''
@@ -14,14 +15,45 @@ class CRSEngine(CRSServer):
         '''
         Constructor
         '''
-        self.local_port = local_port
         try:
             CRSServer.__init__(self,'localhost',self.local_port) 
         except:
             print 'CRSEngine fail creating the XML-RPC server'
         else:
             print 'CRSEngine server listening!!'
-                    
+        '''
+        Properties
+        '''
+        self.delta_time = 0.1 #delta time in sec.
+        self.total_simulation_time = 120.0 #total time of the simulation in sec.
+        self.local_port = local_port
+        
+    def WC_hello(self,param,port,sender='None'):
+        print '[CRSEngine.WC_hello]: %s:%d:%s'%(param,port,sender) 
+        host = 'http://localhost:%d' % port
+        print host
+        if sender.upper() == 'ENV':
+            if not self.Clients.has_key('ENV'):
+                self.Clients[sender] = CRSClient(host,'SERVER','1_0_0_0')
+                return 1
+            else:
+                print '[CRSEngine.WC_hello]: Only one environment is permitted'
+                return 0
+        elif sender.upper() == 'ROBOT':
+            robot_key = 'ROBOT-%05d'%port
+            if not self.Clients.has_key(robot_key):
+                self.Clients[robot_key] = CRSClient(host,robot_key,'1_0_0_0')
+                return 1
+            else:
+                print '[CRSEngine.WC_hello]: This Robot is already registered'
+                return 0            
+    def WC_input(self,port,sender):
+        print '[CRSEngine.WC_input]: ',sender
+        return None
+    def WC_output(self,port,sender,data):
+        print '[CRSEngine.WC_output]: ',sender
+        return 0                    
+
 if __name__ == '__main__':
     # Tester 
     try:    
