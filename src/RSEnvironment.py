a3ff62f4bcefbc32d0110983efb83e92e0055c48
rbulha <rbulha@gmail.com>
2011-01-27 21:02:13 +0000

    load terrain added.

--- src/RSEnvironment.py
+++ src/RSEnvironment.py
@@ -5,6 +5,7 @@ Created on 25/01/2011
 '''
 
 from RSService import CRSService
+from img2raw import CIMGSUPPORT
 
 class CEnvironment(CRSService):
     '''
@@ -14,14 +15,22 @@ class CEnvironment(CRSService):
         '''
         Constructor
         '''
+        
+        self.terrain = None
+        
         try:
             CRSService.__init__(self,'ENV',49581,49580)
         except:
             print 'CEnvironment - XML-RPC server fail'
         else:
             print 'CEnvironment - running'
-
-            
+    def LoadTerrain(self,file_name):        
+        self.terrain = CIMGSUPPORT.BMP2RAW(file_name)
+    def IsReady(self):
+        if not self.terrain:
+            return False
+        else:
+            return True    
 if __name__ == '__main__':
     # Tester 
     try:    
