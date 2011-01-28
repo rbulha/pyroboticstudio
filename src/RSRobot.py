429d383ca58f45add67013f0d6fa0e990c9af668
rbulha <rbulha@gmail.com>
2011-01-28 20:05:41 +0000

    working

--- src/RSRobot.py
+++ src/RSRobot.py
@@ -5,29 +5,45 @@ Created on 26/01/2011
 '''
 
 from RSService import CRSService
+from RSClientServer import CRSClient
 
 class CRSRobot(CRSService):
     '''
     CRSRobot
     '''
-    def __init__(self,local_port,ini_file='none.ini'):
+    def __init__(self,local_port,remote_port=49580,ini_file='none.ini'):
         '''
         Constructor
         '''
         if local_port >= 49582 and local_port <= 49600:
             try:
-                CRSService.__init__(self,'ROBOT',local_port,49580) #local_port from 49582 to 49600
+                CRSService.__init__(self,'ROBOT',local_port,remote_port) #local_port from 49582 to 49600
             except:
                 print 'CRSRobot - XML-RPC server fail'
             else:
                 print 'CRSRobot - running'
         else:
             print 'CRSRobot - running out of ports'
-            
+    def WC_hello(self,param,port,sender='None'):
+        host = 'http://localhost:%d' % port
+        if sender.upper() == 'DSS':
+            if not self.Clients.has_key('DSS'):
+                self.Clients[sender.upper()] = CRSClient(host,'ROBOT','1_0_0_0')
+                return 1
+            else:
+                print '[CRSRobot.WC_hello]: Only one DSS is permitted'
+                return 0        
+    def WC_input(self,port,sender):
+        print '[CRSEngine.WC_input]: ',sender
+        return None
+    def WC_output(self,port,sender,data):
+        print '[CRSEngine.WC_output]: ',sender
+        return 0                    
+           
 if __name__ == '__main__':
     # Tester 
     try:    
-        cService = CRSRobot(49582) 
+        cService = CRSRobot(49582,49580) 
     except:
         print 'CRSRobot - XML-RPC creation fail!'
     else:  
--- src/RSService.py
+++ src/RSService.py
@@ -7,7 +7,7 @@ Created on 25/01/2011
 from RSClientServer import CRSClient
 from RSClientServer import CRSServer
 
-class CRSService(object):
+class CRSService(CRSServer):
     '''
     classdocs
     '''
@@ -15,19 +15,19 @@ class CRSService(object):
         '''
         Constructor
         '''
-        self.service_name = service_name
         try:
-            self.server_port = local_port
-            self.crsServer = CRSServer('localhost',self.server_port) 
-            self.crsServer.start()
+            CRSServer.__init__('localhost',local_port)
+            self.service_name = service_name
+            self.local_port = local_port
         except:
             print '%s fail creating the XML-RPC server'%service_name
         else:
             print '%s server listening!!'%service_name
-                    
+            self.register_function(self.WC_input, "PyRoboticStudio.input")
+            self.register_function(self.WC_output, "PyRoboticStudio.output")
         try:
-            self.engine_server_port = remote_port
-            host = 'http://localhost:%d' % self.engine_server_port
+            self.remote_port = remote_port
+            host = 'http://localhost:%d' % self.remote_port
             self.crsClient = CRSClient(host, service_name, '1_0_0_0')
         except:
             print '%s fail connecting to the server'%service_name
@@ -35,16 +35,17 @@ class CRSService(object):
             print '%s connected!!'%service_name
 
         try:        
-            if not self.crsClient.Hello(self.server_port):
+            if not self.crsClient.Hello(self.local_port):
                 print '%s - Hello not accepted.'%service_name
         except:
             print 'XML-RPC hello fail!'
         else:
             print '%s - Initialization DONE.'%service_name
-    
-    def start(self):
-        '''
-        Initiate service loop
-        '''
-        self.crsServer.join()
+                    
+    def WC_input(self,port,sender):
+        print 'WC_input: ',sender
+        return None
+    def WC_output(self,port,sender,data):
+        print 'WC_output: ',sender
+        return 0
                     
